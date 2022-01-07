1ec1ab2b05dd8268cf74a9b4a944955837f64f3b
Luis Fernando Mercado Paredes <luisfermepa9@gmail.com>
2022-01-06 21:42:35 -0500

    Add files via upload

--- js/cambiarImgs.js
+++ js/cambiarImgs.js
@@ -1,18 +1,32 @@
+var carpetaImgs = "/img/SS";
 var pagina = 1;
+var imgs =[][int];
+var listaImgs=document.getElementById('listaImgs');
+var nodosImg = listaImgs.childNodes;
+
+for(var i=0; i<10; i++){
+    imgs[0][i]=document.getElementById('img'+i);;
+}
 
 function cambiarImg(){
-    var img = document.getElementById("recon1");
-    
+    var img = document.getElementById("img1");
+
     if(pagina==1){
-        img.src="img/SS/recon11.png";
+        img.src=carpetaImgs+"/recon11.png";
         pagina=2;
     } else{
-        img.src="img/SS/recon1.png";
+        img.src=carpetaImgs+"/recon1.png";
         pagina=1;
     }
-    img.src = "img/SS/recon11.png";
+    img.src = carpetaImgs+"/recon11.png";
 }
 
 function irAPagina(numPagina){
-    ;
+    //Guardar imgs actuales
+    for(var i=0; i<nodosImg.length();i++){
+        if(!nodosImg[i].hasChildNodes()){
+            imgs[i]=nodosImg[i];
+        }
+    }
 }
+
