0eaca25e2e9859200bc51f28c65aa13e7450a636
Luis Fernando Mercado Paredes <luisfermepa9@gmail.com>
2022-01-14 18:40:39 -0500

    Add files via upload

--- js/cambiarImgs.js
+++ js/cambiarImgs.js
@@ -1,8 +1,28 @@
+const xhttp = new XMLHttpRequest();
+
 var carpetaImgs = "/img/SS";
 var pagina = 1;
 var imgs =[][int];
 var listaImgs=document.getElementById('listaImgs');
 var nodosImg = listaImgs.childNodes;
+
+xhttp.open('GET', 'infoimg.json', true);
+xhttp.send();
+xhttp.onreadystatechange = function (){
+    if(this.readyState==4 && this.status==200){
+        let datos =  JSON.parse(this.responseText);
+        let res = document.querySelector('#listaImgs');
+        res.innerHTML='';
+
+        for(let item of datos){
+            res.innerHTML += `
+            <div class="row">
+                <img id="img1" class="col s12 l6 responsive-img" src="${item.src}" alt="${item.alt}">
+                <img id="img2" class="col s12 l6 responsive-img" src="${item.src}" alt="${item.alt}">
+            </div>`
+        }
+    }
+}
 
 for(var i=0; i<10; i++){
     imgs[0][i]=document.getElementById('img'+i);;
