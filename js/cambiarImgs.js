c07459d622ede9f4f615beb187109f3cd7a5efa5
Luis Fernando Mercado Paredes <luisfermepa9@gmail.com>
2022-01-18 19:32:57 -0500

    Add files via upload

--- js/cambiarImgs.js
+++ js/cambiarImgs.js
@@ -3,7 +3,45 @@ var pagina = 1;
 var listaImgs=document.getElementById('listaImgs');
 var nodosImg = listaImgs.childNodes;
 
+window.onload = function (){
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', 'json/infoImg.json', true);
+    xhttp.send();
+
+    xhttp.onreadystatechange = function (){
+        if(this.readyState==4 && this.status==200){
+            let datos =  JSON.parse(this.responseText);
+            let res = document.querySelector('#listaImgs');
+            res.innerHTML='';
+            let htmlTxt= '';
+            let fila1 = true;
+            let c=0;
+            for(let item of datos){
+                if(item.prioridad==3 && c<10){
+                    c++;
+                    if(fila1==true){
+                        htmlTxt += `
+                        <div class="row">
+                            <img class="col s12 l6 responsive-img" src="${item.src}" alt="${item.alt}">`
+                    } else{
+                        htmlTxt += `
+                            <img class="col s12 l6 responsive-img" src="${item.src}" alt="${item.alt}">
+                        </div>`
+                        res.innerHTML+=htmlTxt;
+                        htmlTxt='';
+                    }
+                    fila1=!fila1;
+                }
+            }
+        }
+    }
+};
+
 function mostrarImgs(){
+    document.getElementById("Btn2").className = "active blue";
+    document.getElementById("Btn1").className = "waves-effect";
+    document.getElementById("BtnIzquierda").className = "waves-effect";
+    
     const xhttp = new XMLHttpRequest();
     xhttp.open('GET', 'json/infoImg.json', true);
     xhttp.send();
