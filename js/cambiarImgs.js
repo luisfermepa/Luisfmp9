8f0227ae6bcf1a8574c68b81f71d9625e18e7768
Luis Fernando Mercado Paredes <luisfermepa9@gmail.com>
2022-01-20 17:29:29 -0500

    Add files via upload

--- js/cambiarImgs.js
+++ js/cambiarImgs.js
@@ -4,6 +4,18 @@ var listaImgs=document.getElementById('listaImgs');
 var nodosImg = listaImgs.childNodes;
 
 window.onload = function (){
+    mostrarImgsPorPrioridad(3);
+};
+
+function mostrarImgs(val){
+    mostrarImgsPorPrioridad(val);
+}
+
+function mostrarImgsPorPrioridad(prioridad_){
+    document.getElementById("Btn2").className = "active blue";
+    document.getElementById("Btn1").className = "waves-effect";
+    document.getElementById("BtnIzquierda").className = "waves-effect";
+
     const xhttp = new XMLHttpRequest();
     xhttp.open('GET', 'json/infoImg.json', true);
     xhttp.send();
@@ -17,7 +29,7 @@ window.onload = function (){
             let fila1 = true;
             let c=0;
             for(let item of datos){
-                if(item.prioridad==3 && c<10){
+                if(item.prioridad==prioridad_ && c<10){
                     c++;
                     if(fila1==true){
                         htmlTxt += `
@@ -35,65 +47,4 @@ window.onload = function (){
             }
         }
     }
-};
-
-function mostrarImgs(){
-    document.getElementById("Btn2").className = "active blue";
-    document.getElementById("Btn1").className = "waves-effect";
-    document.getElementById("BtnIzquierda").className = "waves-effect";
-    
-    const xhttp = new XMLHttpRequest();
-    xhttp.open('GET', 'json/infoImg.json', true);
-    xhttp.send();
-
-    xhttp.onreadystatechange = function (){
-        if(this.readyState==4 && this.status==200){
-            let datos =  JSON.parse(this.responseText);
-            let res = document.querySelector('#listaImgs');
-            res.innerHTML='';
-            let htmlTxt= '';
-            let fila1 = true;
-            for(let item of datos){
-                if(fila1==true){
-                    htmlTxt += `
-                    <div class="row">
-                        <img class="col s12 l6 responsive-img" src="${item.src}" alt="${item.alt}">`
-                } else{
-                    htmlTxt += `
-                        <img class="col s12 l6 responsive-img" src="${item.src}" alt="${item.alt}">
-                    </div>`
-                    res.innerHTML+=htmlTxt;
-                    htmlTxt='';
-                }
-                fila1=!fila1;
-            }
-        }
-    }
 }
-
-/*
-for(var i=0; i<10; i++){
-    imgs[0][i]=document.getElementById('img'+i);;
-}
-
-function cambiarImg(){
-    var img = document.getElementById("img1");
-
-    if(pagina==1){
-        img.src=carpetaImgs+"/recon11.png";
-        pagina=2;
-    } else{
-        img.src=carpetaImgs+"/recon1.png";
-        pagina=1;
-    }
-    img.src = carpetaImgs+"/recon11.png";
-}
-
-function irAPagina(numPagina){
-    //Guardar imgs actuales
-    for(var i=0; i<nodosImg.length();i++){
-        if(!nodosImg[i].hasChildNodes()){
-            imgs[i]=nodosImg[i];
-        }
-    }
-}*/
