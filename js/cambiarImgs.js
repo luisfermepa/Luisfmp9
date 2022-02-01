cda0440de32943319a8f5b871ea4ebe795089d4d
LuisMercado <luisfermepa9@gmail.com>
2022-01-31 21:18:56 -0500

    viendo tipo de dato de datos

--- js/cambiarImgs.js
+++ js/cambiarImgs.js
@@ -54,6 +54,7 @@ function mostrarImgsPorPagina(pagina){
     xhttp.onreadystatechange = function (){
         if(this.readyState==4 && this.status==200){
             let datos =  JSON.parse(this.responseText);
+            console.log(datos);
             let res = document.querySelector('#listaImgs');
             res.innerHTML='';
             let htmlTxt= '';
