efc49f315e00346d8b117db90c843ce33005329a
Luis Fernando Mercado Paredes <luisfermepa9@gmail.com>
2022-03-31 23:06:42 -0500

    cerrando div en ambos casos

--- js/cambiarImgs.js
+++ js/cambiarImgs.js
@@ -66,7 +66,6 @@ function mostrarImgsPorPagina(pagina){
                         <div class="row">
                             <img class="col s12 l6 responsive-img" src="${item.src}" alt="${item.alt}">
                         `
-                        res.innerHTML+=htmlTxt;
                     } else{
                         htmlTxt += `
                             <img class="col s12 l6 responsive-img" src="${item.src}" alt="${item.alt}">
