74ad1ccae1aa809447e92d848dc8197ed22009ee
Luis Fernando Mercado Paredes <luisfermepa9@gmail.com>
2022-06-16 21:40:40 -0500

    probando funcionamiento formulario

---
+++ js/formulario.js
@@ -0,0 +1,20 @@
+const btnEnvio = document.getElementById('btnEnviar');
+
+btnEnvio.addEventListener('click', function(e){
+    e.preventDefault();
+    const nombres =document.getElementById('nombres').value;
+    const apellidos=document.getElementById('apellidos').value;
+    const nombresCompletos=nombres+apellidos;
+    const email=document.getElementById('email').value;
+    const asuntoDesarrolloWeb=document.getElementById('checkWeb').value;
+    const asuntoVideojuegos=document.getElementById('checkVideojuegos').value;
+    const asuntoTecnologia=document.getElementById('checkTec').value;
+    const asuntoGestion=document.getElementById('checkGestion').value;
+    const asuntosOtros=document.getElementById('otrosAsuntos').value;
+    const asuntoCompleto=asuntoDesarrolloWeb+asuntoVideojuegos+asuntoTecnologia+asuntoGestion+asuntosOtros;
+    const mensaje=document.getElementById('mensaje').value;
+
+    window.location.href=`mailto:luisfermepa9@gmail.com?
+    subject=${asuntoCompleto}&
+    body=Nombre%3A%20${nombresCompletos}%0ACorreo%3A%20${email}%0AMensaje%3A%20${mensaje}`;
+});
