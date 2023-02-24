3756567cdfaa43517f6340fb6596b50657173bdb
Luis Mercado <luisfermepa9@gmail.com>
2023-02-23 19:58:52 -0500

    Translation

--- blogs/modulos_EN.js
+++ blogs/modulos_EN.js
@@ -7,24 +7,24 @@ class Header extends HTMLElement {
                         <div class="nav-wrapper valign-wrapper container blue darken-2">
                             <a href="/" class="brand-logo hide-on-med-and-up">Luisfmp</a>
                             <a href="#" data-target="menu-responsive" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-                            <ul class="left valign-wrapper">
+                            <ul class="valign-wrapper">
                                 <li><a class="hide-on-med-and-down" href="/luisfmp">Luisfmp</a></li>
-                                <li><a class="hide-on-small-only" href="portfolios">Portfolios</a></li>
-                                <li><a class="hide-on-small-only" href="certifications">Certifications</a></li>
-                                <li><a class="hide-on-small-only" href="blogs_EN">Blogs</a></li>
-                                <li><a class="hide-on-med-and-down" href="philosophy">Philosophy</a></li>
-                                <li><a class="hide-on-med-and-down" href="contact">Contact</a></li>
+                                <li><a class="hide-on-small-only" href="/portfolios">Portfolios</a></li>
+                                <li><a class="hide-on-small-only" href="/certifications">Certifications</a></li>
+                                <li><a class="hide-on-small-only" href="/blogs">Blogs</a></li>
+                                <li><a class="hide-on-med-and-down" href="/philosophy">Philosophy</a></li>
+                                <li><a class="hide-on-med-and-down" href="/contact">Contact</a></li>
                             </ul>
                         </div>
                     </nav>
                 </div>
                 <ul class="sidenav" id="menu-responsive">
                     <li><a href="/luisfmp">Luisfmp</a></li>
-                    <li><a href="portfolios">Portfolios</a></li>
-                    <li><a href="certifications">Certifications</a></li>
-                    <li><a href="blogs_EN">Blogs</a></li>
-                    <li><a href="philosophy">Philosophy</a></li>
-                    <li><a href="contact">Contact</a></li>
+                    <li><a href="/portfolios">Portfolios</a></li>
+                    <li><a href="/certifications">Certifications</a></li>
+                    <li><a href="/blogs">Blogs</a></li>
+                    <li><a href="/philosophy">Philosophy</a></li>
+                    <li><a href="/contact">Contact</a></li>
                 </ul>
             </header>
         `
@@ -38,7 +38,7 @@ class Footer extends HTMLElement {
                 <div class="container">
                     <div class="row">
                     <div class="col l6 s12">
-                        <h5 class="white-text">Contact me</h5>
+                        <h5 class="white-text">Contáctame</h5>
                         <p class="grey-text text-lighten-4">You can reach me at these media :)</p>
                         <p>+51 914 609 873</p>
                         <p>luisfermepa9@gmail.com</p>
@@ -58,10 +58,10 @@ class Footer extends HTMLElement {
                     <div class="container">
                         <div class="row valign-wrapper">
                             <div class="col s8">
-                                <p class="grey-text text-lighten-4 left valign-wrapper">© 2022 Luis Fernando Mercado Paredes</p>
+                                <p class="grey-text text-lighten-4 left valign-wrapper">© 2022 Luis Fernando Mercado Predes</p>
                             </div>
                             <div class="col s4">
-                                <a class="grey-text text-lighten-4 left valign-wrapper" href="/luisfmp">https://luisfmp9.github.io/</a>
+                                <a class="grey-text text-lighten-4 left valign-wrapper" href="/luisfmp">luisfmp.com</a>
                             </div>
                         </div>
                     </div>
