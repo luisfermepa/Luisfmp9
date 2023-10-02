3c853bf168f0eb64df4f2dc03e9bd4e1d985fb34
Luis Mercado <luisfermepa9@gmail.com>
2023-10-02 15:34:53 -0500

    Responsive footer

--- js/modulos_EN.js
+++ js/modulos_EN.js
@@ -50,18 +50,18 @@ class Footer extends HTMLElement {
                             <ul>
                                 <li><a class="grey-text text-lighten-3" href="https://www.linkedin.com/in/luisfmp9">Linkedin</a></li>
                                 <li><a class="grey-text text-lighten-3" href="https://luisfmp.itch.io/">Itch (Video Games)</a></li>
-                                <li><a class="grey-text text-lighten-3" href="https://www.facebook.com/luisfmp2">Facebook</a></li>
+                                <li><a class="grey-text text-lighten-3" href="https://www.instagram.com/luisfmp9/">Instagram</a></li>
                                 <li><a class="grey-text text-lighten-3" href="https://www.youtube.com/channel/UCbGQB3Q26ABBMi3_-E3fcEA">Youtube</a></li>
                                 <li><a class="grey-text text-lighten-3" href="/luisfmp">Website</a></li>
                             </ul>
                         </div>
                     </div>
                 </div>
-                <div class="footer-copyright grey darken-4 center">
-                    <div class="container center">
-                        <div class="row center valign-wrapper">
-                            <div class="col s12 center">
-                                <p class="center grey-text text-lighten-4 valign-wrapper">© ${añoActual} Luis Fernando Mercado Paredes</p>
+                <div class="footer-copyright grey darken-4">
+                    <div class="container">
+                        <div class="row valign-wrapper">
+                            <div class="col s8">
+                                <p class="grey-text text-lighten-4 valign-wrapper">© ${añoActual} Luis Fernando Mercado Paredes</p>
                             </div>
                         </div>
                     </div>
